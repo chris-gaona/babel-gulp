e29059239ad8fb9f84107fcaa98e2c54c8715b80
Chris Gaona <thebeloved88@sbcglobal.net>
2016-09-25 09:12:47 -0700

    Adds user data for most recent users & renders it on ui

--- dist/bundle.js
+++ dist/bundle.js
@@ -1106,6 +1106,10 @@ var _post = require("./post");
 
 var _post2 = _interopRequireDefault(_post);
 
+var _user = require("./user");
+
+var _user2 = _interopRequireDefault(_user);
+
 var _ui = require("./ui");
 
 var _ui2 = _interopRequireDefault(_ui);
@@ -1116,7 +1120,11 @@ _post2.default.findAll().then(_ui2.default.renderPosts).catch(function (error) {
   console.log(error);
 });
 
-},{"./post":3,"./ui":4}],3:[function(require,module,exports){
+_user2.default.findRecent().then(_ui2.default.renderUsers).catch(function (error) {
+  console.log(error);
+});
+
+},{"./post":3,"./ui":4,"./user":5}],3:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -1173,6 +1181,17 @@ var ui = {
 
     var target = document.querySelector(".container");
     target.innerHTML = elements.join('');
+  },
+  renderUsers: function renderUsers(users) {
+    var elements = users.map(function (user) {
+      var name = user.name;
+      var avatar = user.avatar;
+
+      return userTemplate(name, avatar);
+    });
+
+    var target = document.querySelector('.sidebar-content');
+    target.innerHTML = elements.join('');
   }
 };
 
@@ -1184,6 +1203,45 @@ function articleTemplate(title, lastReply) {
   return template;
 }
 
+function userTemplate(name, avatar) {
+  var safeName = _xssFilters2.default.inHTMLData(name);
+  var safeAvatar = _xssFilters2.default.inHTMLData(avatar);
+
+  var template = "\n      <div class=\"active-avatar\">\n      <img width=\"54\" src=\"assets/images/" + safeAvatar + "\">\n      <h5 class=\"post-author\">" + safeName + "</h5>\n      </div>";
+
+  return template;
+}
+
 exports.default = ui;
 
-},{"xss-filters":1}]},{},[2]);
+},{"xss-filters":1}],5:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var User = {
+  findRecent: function findRecent() {
+    return new Promise(function (resolve, reject) {
+      var uri = "http://localhost:3000/activeUsers";
+      var request = new XMLHttpRequest();
+
+      request.open('GET', uri, true);
+      request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+          resolve(JSON.parse(request.response));
+        }
+      };
+
+      request.onerror = function () {
+        reject(new Error('Something went wrong on the API'));
+      };
+
+      request.send();
+    });
+  }
+};
+
+exports.default = User;
+
+},{}]},{},[2]);
