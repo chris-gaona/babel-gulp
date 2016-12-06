7f1b21a59448a1d7ffd95c4318ef02d603636091
Chris Gaona <thebeloved88@sbcglobal.net>
2016-12-06 09:42:15 -0800

    Follows github issue post to fix issues on Heroku

--- server/server.js
+++ server/server.js
@@ -1,50 +1,8 @@
-// server.js
 var jsonServer = require('json-server');
 var server = jsonServer.create();
-var db = {
-    posts: [
-        {
-            "id": 1,
-            "title": "In hybrid moments, give me a moment",
-            "lastReply": "1997-07-15"
-        },
-        {
-            "id": 2,
-            "title": "Sound system gonna bring me back up",
-            "lastReply": "1991-07-01"
-        },
-        {
-            "id": 3,
-            "title": "When I'm out walkin', I strut my stuff",
-            "lastReply": "1983-04-19"
-        }
-    ],
-    activeUsers: [
-        {
-            "name": "Sam",
-            "avatar": "avatar.jpg"
-        },
-        {
-            "name": "Tyler",
-            "avatar": "avatar.jpg"
-        },
-        {
-            "name": "Brook",
-            "avatar": "avatar.jpg"
-        }
-    ]
-};
-
-var router;
-if (process.env.NODE_ENV === 'development') {
-    router = jsonServer.router('server/db.json');
-} else {
-    router = jsonServer.router(db);
-}
-
+var router = jsonServer.router('server/db.json');
 var middlewares = jsonServer.defaults();
-var port = process.env.PORT || 3000;
-
+var port = Number(process.env.PORT || 3000);
 server.use(middlewares);
 server.use(router);
 server.listen(port, function () {
