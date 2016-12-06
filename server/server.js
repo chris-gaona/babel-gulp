b5f0f103fad2119ba54b0953a3ed296d32111a8f
Chris Gaona <thebeloved88@sbcglobal.net>
2016-12-06 09:35:20 -0800

    Uses in memory storage with json-server on Heroku to fix issues with serving api over http w/out the s

--- server/server.js
+++ server/server.js
@@ -1,7 +1,47 @@
 // server.js
 var jsonServer = require('json-server');
 var server = jsonServer.create();
-var router = jsonServer.router('server/db.json');
+var db = {
+    posts: [
+        {
+            "id": 1,
+            "title": "In hybrid moments, give me a moment",
+            "lastReply": "1997-07-15"
+        },
+        {
+            "id": 2,
+            "title": "Sound system gonna bring me back up",
+            "lastReply": "1991-07-01"
+        },
+        {
+            "id": 3,
+            "title": "When I'm out walkin', I strut my stuff",
+            "lastReply": "1983-04-19"
+        }
+    ],
+    activeUsers: [
+        {
+            "name": "Sam",
+            "avatar": "avatar.jpg"
+        },
+        {
+            "name": "Tyler",
+            "avatar": "avatar.jpg"
+        },
+        {
+            "name": "Brook",
+            "avatar": "avatar.jpg"
+        }
+    ]
+};
+
+var router;
+if (process.env.NODE_ENV === 'development') {
+    router = jsonServer.router('server/db.json');
+} else {
+    router = jsonServer.router(db);
+}
+
 var middlewares = jsonServer.defaults();
 var port = process.env.PORT || 3000;
 
